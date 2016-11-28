461cb577758457b97dc38e8a5d673fc0488801bb
jay0932 <jay0932business@gmail.com>
2016-11-29 02:13:42 +0800

    add shops migration

--- db/migrate/20161128180037_add_shops.rb
+++ db/migrate/20161128180037_add_shops.rb
@@ -1,4 +1,18 @@
 class AddShops < ActiveRecord::Migration[5.0]
   def change
+  	create_table :users do |t|
+      t.integer :user_id
+      t.string  :city
+      t.string  :market_name
+      t.string  :address
+      t.datetime  :rent_start_datetime
+      t.datetime  :rent_end_datetime
+      t.float :rent_prices
+      t.string :rent_status
+      t.text   :shop_tool
+      t.text   :shop_note
+
+      t.timestamps null: false
+    end
   end
 end
