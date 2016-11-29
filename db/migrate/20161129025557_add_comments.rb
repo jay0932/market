1e7f047f249a43663f9cd8f6958c605ba1209993
jay0932 <jay0932business@gmail.com>
2016-11-29 10:57:50 +0800

    add comments migration

--- db/migrate/20161129025557_add_comments.rb
+++ db/migrate/20161129025557_add_comments.rb
@@ -1,4 +1,10 @@
 class AddComments < ActiveRecord::Migration[5.0]
   def change
+  	 create_table :comments do |t|
+      t.integer :user_id
+      t.text  :content
+
+      t.timestamps null: false
+    end
   end
 end
