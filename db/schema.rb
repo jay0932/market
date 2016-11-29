aa2d3be9af4dadd03bae6250a9518f09c9b1ba0a
jay0932 <jay0932business@gmail.com>
2016-11-29 15:51:01 +0800

    add orders column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129074502) do
+ActiveRecord::Schema.define(version: 20161129074825) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20161129074502) do
     t.datetime "order_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "shop_id"
   end
 
   create_table "rates", force: :cascade do |t|
