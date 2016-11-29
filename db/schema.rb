f6542b8552d1e2cb7a97ed9b82705166011a6d2c
jay0932 <jay0932business@gmail.com>
2016-11-29 10:58:40 +0800

    add comments schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129024453) do
+ActiveRecord::Schema.define(version: 20161129025557) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
