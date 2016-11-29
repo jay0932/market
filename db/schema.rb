c07f8d3de115017102f5f413da7b4cd2a8c11e1a
jay0932 <jay0932business@gmail.com>
2016-11-29 10:55:01 +0800

    add orders schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128180037) do
+ActiveRecord::Schema.define(version: 20161129024453) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "status"
+    t.datetime "order_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.integer  "user_id"
