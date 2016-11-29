7b60addf5cb578c7d365565712d5f7328fc961a2
jay0932 <jay0932business@gmail.com>
2016-11-29 10:53:06 +0800

    add orders migration

--- db/migrate/20161129024453_add_orders.rb
+++ db/migrate/20161129024453_add_orders.rb
@@ -1,4 +1,12 @@
 class AddOrders < ActiveRecord::Migration[5.0]
   def change
+  	def change
+    create_table :orders do |t|
+      t.integer :user_id
+      t.string  :status
+      t.datetime :order_time
+
+      t.timestamps null: false
+    end
   end
 end
